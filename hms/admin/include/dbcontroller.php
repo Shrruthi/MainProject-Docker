f1201d01e96308c5302fd8edd795a4459b4d7a9a
Sanjay_kumar <sanjaykumar10.sk98@gmail.com>
2019-04-16 23:38:01 +0530

    db changes

--- hms/admin/include/dbcontroller.php
+++ hms/admin/include/dbcontroller.php
@@ -1,8 +1,8 @@
 <?php
-$DB_host = "localhost";
-$DB_user = "root";
-$DB_pass = "";
-$DB_name = "ingram";
+$DB_host = "projectdb1.cbqwym87o1nl.ap-south-1.rds.amazonaws.com";
+$DB_user = "projectdb1";
+$DB_pass = "projectdb1";
+$DB_name = "projectdb1";
 try
 {
  $DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass);
